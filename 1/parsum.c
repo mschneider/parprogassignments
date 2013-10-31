2f68f9836bd0a3a13029a8ee35185d3c4610a2d5
Maximilian Schneider <mail@maximilianschneider.net>
2013-10-31 01:30:06 +0100

    naive implementation & runtime measurement

--- 1/parsum.c
+++ 1/parsum.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <sys/time.h>
 
 typedef struct arg_t
 {
-  int threads;
-  int start;
-  int end;
+  unsigned int threads;
+  uint64_t start;
+  uint64_t end;
 } arg_t;
+typedef struct timeval timeval;
 
 void read_args(
-    char** argv, // in
-    arg_t* args) // out
+  char** argv, // in
+  arg_t* args) // out
 {
-  args->threads = atoi(argv[1]);
-  args->start = atoi(argv[2]);
-  args->end = atoi(argv[3]);
+  sscanf(argv[1], "%d", &args->threads);
+  sscanf(argv[2], "%lld", &args->start);
+  sscanf(argv[3], "%lld", &args->end);
+}
+
+uint64_t sum_naive(
+  uint64_t start,
+  uint64_t end)
+{
+  uint64_t result = 0;
+  while(start <= end)
+  {
+    result += start;
+    start += 1;
+  }
+  return result;
+}
+
+uint64_t sum_gauss(uint64_t limit)
+{
+  return limit * (limit+1) / 2;
+}
+
+double timediff(timeval a, timeval b)
+{
+  return 1.0 * (b.tv_sec - a.tv_sec)
+       + 0.000001 * (b.tv_usec - a.tv_usec);
 }
 
 int main(int argc, char** argv)
 {
   arg_t args;
+  timeval start, end;
 
   if (argc != 4)
   {
@@ -27,8 +55,17 @@ int main(int argc, char** argv)
   }
 
   read_args(argv, &args);
-  printf("running with %d threads from %d to %d max",
+  printf("running with %u threads from %llu to %llu max\n",
          args.threads, args.start, args.end);
+
+  gettimeofday(&start, NULL);
+  uint64_t result = sum_naive(args.start, args.end);
+  gettimeofday(&end, NULL);
+  printf("calculated result:\t%llu in %fs\n",
+         result, timediff(start, end));
+
+  uint64_t check = sum_gauss(args.end) - sum_gauss(args.start - 1);
+  printf("calculated check:\t%lld\n", check);
 
   return 0;
 }
