b58864b8770ba3418d74280ff2f5fa8e8779783f
Maximilian Schneider <mail@maximilianschneider.net>
2013-11-01 23:19:17 +0100

    handcrafted loop unrolling, because gcc won't do it

--- 1/parsum.c
+++ 1/parsum.c
@@ -32,6 +32,42 @@ uint64_t sum_naive(
   return result;
 }
 
+uint64_t sum_shift(
+  uint64_t start,
+  uint64_t end)
+{
+  uint64_t result = 0;
+  while (start < end)
+  {
+    result += start << 1;
+    result += 1;
+    start += 2;
+  }
+
+  if (start == end)
+    result += start;
+
+  return result;
+}
+
+uint64_t sum_more_shift(
+  uint64_t start,
+  uint64_t end)
+{
+  uint64_t result = 0;
+  if (end >= 16)
+  {
+    uint64_t unrolled_end = end - 16;
+    while (start <= unrolled_end)
+    {
+      result += start << 4;
+      result += 1+2+3+4+5+6+7+8+9+10+11+12+13+14+15;
+      start += 16;
+    }
+  }
+  return result + sum_naive(start, end);
+}
+
 uint64_t sum_gauss(uint64_t limit)
 {
   return limit * (limit+1) / 2;
@@ -53,13 +89,25 @@ int main(int argc, char** argv)
          args.threads, args.start, args.end);
 
   gettimeofday(&start, NULL);
-  uint64_t result = sum_naive(args.start, args.end);
+  uint64_t naive_result = sum_naive(args.start, args.end);
   gettimeofday(&end, NULL);
-  printf("calculated result:\t%llu in %fs\n",
-         result, timediff(start, end));
+  printf("naive result:\t%llu in %fs\n",
+         naive_result, timediff(start, end));
+
+  gettimeofday(&start, NULL);
+  uint64_t shift_result = sum_shift(args.start, args.end);
+  gettimeofday(&end, NULL);
+  printf("shift result:\t%llu in %fs\n",
+         shift_result, timediff(start, end));
+
+  gettimeofday(&start, NULL);
+  uint64_t more_shift_result = sum_more_shift(args.start, args.end);
+  gettimeofday(&end, NULL);
+  printf("++shift result:\t%llu in %fs\n",
+         more_shift_result, timediff(start, end));
 
   uint64_t check = sum_gauss(args.end) - sum_gauss(args.start - 1);
-  printf("calculated check:\t%lld\n", check);
+  printf("check sum:\t%lld\n", check);
 
   return 0;
 }
