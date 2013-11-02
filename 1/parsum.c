212f6c9acf0da9f29f7b1eff2d98e98fc3c73644
Maximilian Schneider <mail@maximilianschneider.net>
2013-11-02 08:08:57 +0100

    added unrolled versions not using shift shortcut

--- 1/parsum.c
+++ 1/parsum.c
@@ -68,6 +68,68 @@ uint64_t sum_more_shift(
   return result + sum_naive(start, end);
 }
 
+uint64_t sum_unrolled_8(
+  uint64_t start,
+  uint64_t end)
+{
+  uint64_t result = 0;
+  if (end >= 8)
+  {
+    uint64_t unrolled_end = end - 8;
+    while (start <= unrolled_end)
+    {
+      result += start;
+      result += start + 1;
+      result += start + 2;
+      result += start + 3;
+      result += start + 4;
+      result += start + 5;
+      result += start + 6;
+      result += start + 7;
+      start += 8;
+    }
+  }
+  return result + sum_naive(start, end);
+}
+
+
+
+
+
+uint64_t sum_unrolled_16(
+  uint64_t start,
+  uint64_t end)
+{
+  uint64_t result = 0;
+  if (end >= 16)
+  {
+    uint64_t unrolled_end = end - 16;
+    while (start <= unrolled_end)
+    {
+      result += start;
+      result += start + 1;
+      result += start + 2;
+      result += start + 3;
+      result += start + 4;
+      result += start + 5;
+      result += start + 6;
+      result += start + 7;
+      result += start + 8;
+      result += start + 9;
+      result += start + 10;
+      result += start + 11;
+      result += start + 12;
+      result += start + 13;
+      result += start + 14;
+      result += start + 15;
+      start += 16;
+    }
+  }
+  return result + sum_naive(start, end);
+}
+
+
+
 uint64_t sum_gauss(uint64_t limit)
 {
   return limit * (limit+1) / 2;
@@ -105,6 +167,19 @@ int main(int argc, char** argv)
   gettimeofday(&end, NULL);
   printf("++shift result:\t%llu in %fs\n",
          more_shift_result, timediff(start, end));
+
+  gettimeofday(&start, NULL);
+  uint64_t unrol8_result = sum_unrolled_8(args.start, args.end);
+  gettimeofday(&end, NULL);
+  printf("unrol8 result:\t%llu in %fs\n",
+         unrol8_result, timediff(start, end));
+
+  gettimeofday(&start, NULL);
+  uint64_t unrol16_result = sum_unrolled_16(args.start, args.end);
+  gettimeofday(&end, NULL);
+  printf("unrol16 result:\t%llu in %fs\n",
+         unrol16_result, timediff(start, end));
+
 
   uint64_t check = sum_gauss(args.end) - sum_gauss(args.start - 1);
   printf("check sum:\t%lld\n", check);
