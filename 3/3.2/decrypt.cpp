d3a030db55606043de8028ae9691684c5f21883c
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2013-12-06 16:20:50 +0100

    Decrypt Versuch

--- 3/3.2/decrypt.cpp
+++ 3/3.2/decrypt.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
+#include <cstring>
 
 #include <map>
+#include <vector>
 #include <set>
-
+#include <unistd.h>
 #include <functional>
+#include <omp.h>
 
-typedef std::set<std::string> Dictionary;
+
+using namespace std;
+
+//Optimierungspotenzial Vector zum iterieren und 
+
+
+typedef set<string> Dictionary;
 
 struct Password
 {
-    std::string salt, cyphertext, plaintext;
+    string salt, cyphertext, plaintext;
 
-    Password(){}
+    Password(){} //wozu?!?!
 
-    Password(const std::string & hash)
-    : salt(hash.substr(0,2))
+    Password(const string & hash)
+      : salt(hash.substr(0,2))
     , cyphertext(hash.substr(2))
     {}
 };
 
-typedef std::map<std::string, Password> UserPasswords;
+typedef map<string, Password> UserPasswords;
 
-bool read_line_by_line(const std::string & filename, std::function<bool (std::string)> callback)
+bool read_line_by_line(const string & filename, function<bool (string)> callback)
 {
-    std::ifstream input(filename);
-    std::string line;
-    while (std::getline(input, line))
+    ifstream input(filename);
+    string line;
+    while (getline(input, line))
     {
         if (! callback(line))
             return false;
@@ -37,24 +45,24 @@ bool read_line_by_line(const std::string & filename, std::function<bool (std::st
 };
 
 bool read_password_file(
-    const std::string & filename, // in
+    const string & filename, // in
     UserPasswords & passwords) // out
 {
-    return read_line_by_line(filename, [&passwords](const std::string & line)
+    return read_line_by_line(filename, [&passwords](const string & line)
     {
-        std::istringstream line_as_stream(line);
+        istringstream line_as_stream(line);
 
-        std::string username;
-        if (! std::getline(line_as_stream, username, ':'))
+        string username;
+        if (! getline(line_as_stream, username, ':'))
         {
-            std::cerr << "wrong line format, could not read username" << std::endl;
+            cerr << "wrong line format, could not read username" << endl;
             return false;
         }
 
-        std::string hash_part_of_line;
-        if (! std::getline(line_as_stream, hash_part_of_line, '\r'))
+        string hash_part_of_line;
+        if (! getline(line_as_stream, hash_part_of_line, '\r'))
         {
-            std::cerr << "wrong line format, could not read password" << std::endl;
+            cerr << "wrong line format, could not read password" << endl;
             return false;
         }
 
@@ -64,10 +72,10 @@ bool read_password_file(
 }
 
 bool read_dictionary_file(
-    const std::string & filename, // in
+    const string & filename, // in
     Dictionary & dictionary) // out
 {
-    return read_line_by_line(filename, [&dictionary](const std::string & line)
+    return read_line_by_line(filename, [&dictionary](const string & line)
     {
         dictionary.insert(line);
         return true;
@@ -78,51 +86,119 @@ void write_decrypted_passwords(
     const UserPasswords & passwords)
 {
     auto passwords_decrypted = 0;
-    std::ofstream output("output.txt");
+    ofstream output("output.txt");
     for (auto & entry : passwords)
     {
         auto & password = entry.second;
         if (! password.plaintext.empty())
         {
             auto & username = entry.first;
-            output << username << ';' << password.plaintext << std::endl;
+            output << username << ';' << password.plaintext << endl;
             passwords_decrypted += 1;
         }
     }
-    std::cout << "Decrypted " << passwords_decrypted << " passwords" << std::endl;
+    cout << "Decrypted " << passwords_decrypted << " passwords" << endl;
+}
 
+
+void set_found_passwords (string username,string password,UserPasswords & passwords)
+{
+  passwords[username].plaintext = password;
 
 }
+
+
+void crack_passwords(UserPasswords &passwords, Dictionary &pw_dictionary)
+  {
+
+   int found_pass_count=0; // Können abbrechen sobald 2 Passwörter geefunden
+
+   for (int i=0;i<passwords.size();i++) //damit parallelisierbar
+      {
+     for (int j=0;j<pw_dictionary.size();j++)
+       {
+            map<string, Password>::iterator pass_user_it=next(passwords.begin(),i);
+            set<string>::iterator dict_it=next(pw_dictionary.begin(),j);
+
+            string crypted_dict_entry=crypt((const char *)&passwords[pass_user_it->first].salt,(const char *) &pw_dictionary); // Doesn't work yet get Pointer how to change to string ?!
+
+             if(strcmp(crypted_dict_entry,(passwords[pass_user_it->first].salt+passwords[pass_user_it->first].cyphertext))
+                {
+                 passwords[pass_user_it->first].plaintext =*dict_it;
+                 found_pass_count++;
+
+                 break;
+                 }
+             else
+                {
+                for(int k=0;k<=9;k++)
+                 {
+                 string passconcat = *dict_it + to_string(k);
+                 crypted_dictionary_entry =  crypt((const char *)&passwords[pass_user_it->first].salt,(const char *) &passconcat);
+
+                 if(strcmp(crypted_dict_entry,passwords[pass_user_it->first].cyphertext))
+                    {
+                      passwords[pass_user_it->first].plaintext = passconcat;
+                      found_pass_count++;
+                      break; //wie aus zweiter Schelife dei geschachtelt ist raus kommen
+                    }
+
+                 }
+                }
+         }
+        }
+   if(found_pass_count==2) //Wehn two password found exit Function
+     return; 
+
+  }
+
+
+
+
 
 int main(int argc, char** argv)
 {
     if (argc < 3)
     {
-        std::cerr << "usage: decrypt <password_file> <dictionary_file>" << std::endl;
+        cerr << "usage: decrypt <password_file> <dictionary_file>" << endl;
         return -1;
     }
 
+
     UserPasswords passwords_by_username;
     Dictionary possible_keys;
+    map<string, Password>::iterator pass_user_it = passwords_by_username.begin();
+   // set<string>::iterator dict_it= possible_keys.begin();
+    int passwortcounter;
+ 
     bool ok = read_password_file(argv[1], passwords_by_username);
     if (! ok || passwords_by_username.empty())
     {
-        std::cerr << "could not read password_file" << std::endl;
+        cerr << "could not read password_file" << endl;
         return -1;
     }
-    std::cout << "PasswordDatabase contains " << passwords_by_username.size() << " users" << std::endl;
+    cout << "PasswordDatabase contains " << passwords_by_username.size() << " users" << endl;
 
     ok = read_dictionary_file(argv[2], possible_keys);
     if (! ok || possible_keys.empty())
     {
-        std::cerr << "could not read dictionary_file" << std::endl;
+        cerr << "could not read dictionary_file" << endl;
         return -1;
     }
-    std::cout << "Dictionary contains " << possible_keys.size() << " possible keys" << std::endl;
+    cout << "Dictionary contains " << possible_keys.size() << " possible keys" << endl;
 
-    // decrypt passwords and write decrypted passwords into password.plain_text
-    passwords_by_username["User01"].plaintext = "pass";
+    //Define Iterators for Map and Dictionary
+    //
+    //
+    //
 
+   // string encrypt = crypt(((const string &) dict_it),"iu");
+   //               cout << *dict_it <<" " << encrypt << "\n" << (&dict_it)  << endl;
+
+
+
+
+    // decrypt passwords and write decrypted passwords into password.plain_text wie kommt das Ergebnis in die File?!?!
     write_decrypted_passwords(passwords_by_username);
     return 0;
 }
